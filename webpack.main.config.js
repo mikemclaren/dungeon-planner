84fc47fea08aea1ea92aae4db7f9aa50494a6f10
Mike Mclaren <rastalinmitomo@gmail.com>
2021-04-28 17:04:31 -0500

    Update webpack.main.config.js

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,17 +1,18 @@
-const path = require("path");
+const path = require('path');
+const rules = require('./webpack.rules');
 
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   // Put your normal webpack config below here
   module: {
-    rules: require("./webpack.rules"),
+    rules,
   },
   resolve: {
-    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
-    modules: [path.resolve(__dirname, "node_modules"), "node_modules"],
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+    modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
   },
 };
